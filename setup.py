e65f8564123f8400d0bc87c27edf01f5fd89655f
jhylands <jhylands@cassinisystems.com>
2020-12-04 18:49:08 +0000

    Made into package
    Tried avoiding making a package but it seems this is the easiest way to get the tests to run

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+distribution_name = 'feed'
+
+setup(
+    name='feed',
+    version='0.1',
+    author='jhylands',
+    author_email='hylands.james@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    package_data={},
+    include_package_data=False,
+    entry_points={},
+)
+
---
+++ test/test_item_manager.py
@@ -0,0 +1,4 @@
+from app.item_manager import get_items
+
+def test_get_items():
+    items = get_items()
