98ade38babc846ad73bd67af4c357a610809a113
jhylands <jhylands@cassinisystems.com>
2020-12-07 22:29:56 +0000

    Trying to make youtube more generic

--- worker/youtube.py
+++ worker/youtube.py
@@ -2,8 +2,8 @@ from worker.feed import Feed
 
 
 class Youtube(Feed):
-    def __init__(self):
-        self.url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCZFipeZtQM5CKUjx6grh54g"
+    def __init__(self, channel_id):
+        self.url = "https://www.youtube.com/feeds/videos.xml?channel_id=" + channel_id
 
     @staticmethod
     def get_entries(xml):
@@ -20,3 +20,17 @@ class Youtube(Feed):
     @staticmethod
     def get_link(entry):
         return entry.link["href"]
+
+IssacArther = "UCZFipeZtQM5CKUjx6grh54g"
+RobertMiles = "UCLB7AzTwc6VFZrBsO2ucBMg"
+UpAndAtom = "UCSIvk78tK2TiviLQn4fSHaw"
+SabineHossenfelder = "UC1yNl2E66ZzKApQdRuTQ4tw"
+vsauce = "UC6nSFpj9HTCZ5t-N3Rm3-HA"
+theCodingTrain = "UCvjgXvBlbQiydffZU7m1_aw"
+Integza = "UC2avWDLN1EI3r1RZ_dlSxCw"
+
+"""
+Computerphile
+Numberphile
+Numberphile2
+"""
